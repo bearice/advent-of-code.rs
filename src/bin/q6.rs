2304ccd14fcf37f4ae5c63114b11c6505718c9f8
Bearice Renbearice@icybear.net <bearice@icybear.net>
2020-12-18 22:13:13 +0800

    quest 6

---
+++ src/bin/q6.rs
@@ -0,0 +1,25 @@
+use advent_of_code::common::read_lines;
+use std::collections::HashSet;
+
+fn main() {
+    let lines = read_lines("./input6.txt").unwrap();
+    let mut cnt = 0;
+    let mut buf: Option<HashSet<char>> = None;
+    for line in lines {
+        let line = line.unwrap();
+        if line.len() > 0 {
+            let a = line.chars().collect();
+            if let Some(b) = buf {
+                buf = Some(b.intersection(&a).cloned().collect());
+            } else {
+                buf = Some(a);
+            }
+        } else {
+            let b = buf.unwrap_or_default();
+            println!("{}", b.len());
+            cnt += b.len();
+            buf = None;
+        }
+    }
+    println!("{}", cnt);
+}
