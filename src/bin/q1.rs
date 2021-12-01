51eae465f8a860c356ecc4751142322215dcecde
Bearice Ren <bearice@icybear.net>
2021-12-01 17:08:05 +0800

    2021-q1

--- src/bin/q1.rs
+++ src/bin/q1.rs
@@ -1,29 +1,29 @@
 use advent_of_code::common::read_lines;
 fn main() {
-    let mut v: Vec<usize> = read_lines("./input1.txt")
+    let v: Vec<i32> = read_lines("./input1.txt")
         .unwrap()
         .map(|x| x.unwrap().parse().unwrap())
         .collect();
-    v.sort();
-    // println!("{:?}", v);
-    let mut x = 0;
-    let mut i = v.iter();
-    while let Some(a) = i.next() {
-        let mut i2 = i.clone();
-        while let Some(b) = i2.next() {
-            if a + b >= 2020 {
-                break;
+
+    fn count_incr(v: impl Iterator<Item = i32>) -> i32 {
+        v.fold((-1, 0), |(n, last), x| {
+            // println!("n={} last={} x={}", n, last, x);
+            if n == -1 {
+                (0, x)
+            } else if x > last {
+                (n + 1, x)
+            } else {
+                (n, x)
             }
-            let mut i3 = i2.clone();
-            while let Some(c) = i3.next() {
-                x = x + 1;
-                if a + b + c == 2020 {
-                    println!("{}", a * b * c);
-                } else if a + b + c > 2020 {
-                    break;
-                }
-            }
-        }
+        })
+        .0
     }
-    // println!("{},{}", x, v.len());
+    let a1 = count_incr(v.iter().copied());
+    println!("a1={}", a1);
+    let v1 = v.iter();
+    let v2 = v.iter().zip(v1.skip(1));
+    let v3 = v.iter().zip(v2.skip(1));
+    let vs = v3.map(|(x, (y, z))| x + y + z);
+    let a2 = count_incr(vs);
+    println!("a2={}", a2);
 }
