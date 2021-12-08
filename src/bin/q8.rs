bac6aba7d4a34a8c25d82f00ae6d9a0076a865b5
Bearice Ren <bearice@icybear.net>
2021-12-08 15:22:17 +0800

    2021-q8

--- src/bin/q8.rs
+++ src/bin/q8.rs
@@ -1,85 +1,97 @@
-use std::{collections::HashSet, str::FromStr};
+use std::collections::HashSet;
 
 use advent_of_code::common::read_lines;
 
-#[derive(Debug, Clone, Copy)]
-enum Instruction {
-    Nop(isize),
-    Acc(isize),
-    Jmp(isize),
-}
-
-impl Instruction {
-    fn exec(&self, acc: &mut isize, pc: isize) -> isize {
-        match self {
-            Self::Nop(_) => pc + 1,
-            Self::Acc(x) => {
-                *acc += x;
-                pc + 1
-            }
-            Self::Jmp(x) => pc + x,
-        }
-    }
-    fn arg(&self) -> isize {
-        *match self {
-            Self::Nop(x) => x,
-            Self::Acc(x) => x,
-            Self::Jmp(x) => x,
-        }
-    }
-}
-impl FromStr for Instruction {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut v = s.split_ascii_whitespace();
-        let cmd = v.next().unwrap();
-        let arg = v.next().unwrap().parse().unwrap();
-        match cmd {
-            "nop" => Ok(Self::Nop(arg)),
-            "jmp" => Ok(Self::Jmp(arg)),
-            "acc" => Ok(Self::Acc(arg)),
-            _ => Err(s.to_owned()),
-        }
-    }
-}
-
-fn check(instructions: &Vec<Instruction>) -> (isize, isize) {
-    // println!("{:?}", instructions);
-    let mut pc: isize = 0;
-    let mut acc: isize = 0;
-    let mut visited = HashSet::new();
-    while !visited.contains(&pc) && (pc as usize) < instructions.len() {
-        visited.insert(pc);
-        let i = &instructions[pc as usize];
-        // println!("{} {:?}", pc, i);
-        pc = i.exec(&mut acc, pc);
-        // println!("{:?}", visited);
-    }
-    // println!("{}", acc);
-    (pc, acc)
-}
-
 fn main() {
-    let mut instructions: Vec<_> = read_lines("./input8.txt")
-        .map(|s| Instruction::from_str(&s))
-        .map(Result::unwrap)
-        .collect();
+    let input = read_lines("input8.txt").collect::<Vec<_>>();
+    let count = input
+        .iter()
+        .map(|s| {
+            s.split_once('|')
+                .unwrap()
+                .1
+                .split_whitespace()
+                .filter(|w| w.len() == 2 || w.len() == 3 || w.len() == 4 || w.len() == 7)
+                .count()
+        })
+        .sum::<usize>();
+    println!("{}", count);
+    let sum = input.iter().map(|s| decode(s.as_str())).sum::<u32>();
+    println!("{}", sum);
+}
 
-    for i in 0..instructions.len() {
-        let old = instructions[i];
-        let new = match &old {
-            Instruction::Jmp(x) => Instruction::Nop(*x),
-            Instruction::Nop(x) => Instruction::Jmp(*x),
-            _ => continue,
-        };
-        instructions[i] = new;
-        let ret = check(&instructions);
-        // println!("{:?}", ret);
-        if ret.0 as usize == instructions.len() {
-            println!("i={} inst={:?} acc={}", i, old, ret.1);
-            break;
+fn decode(input: &str) -> u32 {
+    let (sample, output) = input.split_once('|').unwrap();
+    let samples = sample.split_whitespace().collect::<Vec<_>>();
+    let mut wires = [""; 10];
+    for s in samples.iter() {
+        match s.len() {
+            2 => wires[1] = s,
+            3 => wires[7] = s,
+            4 => wires[4] = s,
+            7 => wires[8] = s,
+            _ => (),
         }
-        instructions[i] = old;
     }
+    // let _A = diff(wires[7].chars(), wires[1].chars());
+    let bd = diff(wires[4].chars(), wires[1].chars());
+    let abcdf = union(wires[4].chars(), wires[7].chars());
+    wires[9] = find(samples.clone(), &abcdf, 6);
+    let g = diff(wires[9].chars(), abcdf.into_iter());
+    let e = diff(wires[8].chars(), wires[9].chars());
+    let cfeg = union(vec![g[0], e[0]], wires[1].chars());
+    wires[0] = find(samples.iter().cloned().filter(|s| *s != wires[9]), &cfeg, 6);
+    let cf = wires[1].chars().collect::<Vec<_>>();
+    wires[3] = find(samples.clone(), &cf, 5);
+    let bed = union(bd, e.clone());
+    wires[6] = find(samples.clone(), &bed, 6);
+    let c = diff(wires[8].chars(), wires[6].chars());
+    let ce = vec![c[0], e[0]];
+    wires[2] = find(samples.clone(), &ce, 5);
+    let bdf = diff(wires[4].chars(), c);
+    wires[5] = find(samples.clone(), &bdf, 5);
+    // println!("{:?}", wires);
+    let outputs = output.split_whitespace().collect::<Vec<_>>();
+    let mut result = 0;
+    for (i, o) in outputs.iter().enumerate() {
+        for j in 0..10 {
+            let t = wires[j as usize];
+            if o.len() == t.len() && o.chars().all(|c| t.contains(c)) {
+                // println!("{}={}", o, j);
+                result += j * 10_u32.pow((outputs.len() - i - 1) as u32);
+            }
+        }
+    }
+    result
+}
+
+fn find<'a>(
+    samples: impl IntoIterator<Item = &'a str>,
+    contains: &[char],
+    length: usize,
+) -> &'a str {
+    samples
+        .into_iter()
+        .find(|s| s.len() == length && contains_all(s, contains))
+        .unwrap()
+}
+
+fn diff(a: impl IntoIterator<Item = char>, b: impl IntoIterator<Item = char>) -> Vec<char> {
+    let b: HashSet<_> = b.into_iter().collect();
+    a.into_iter().filter(|c| !b.contains(c)).collect()
+}
+
+fn union(a: impl IntoIterator<Item = char>, b: impl IntoIterator<Item = char>) -> Vec<char> {
+    let mut set = HashSet::new();
+    for c in a {
+        set.insert(c);
+    }
+    for c in b {
+        set.insert(c);
+    }
+    set.into_iter().collect()
+}
+
+fn contains_all(s: &str, chars: &[char]) -> bool {
+    chars.iter().all(|c| s.contains(*c))
 }
