ed18e73ccff31f6cd60af97c6204041a70cb6e2b
Bearice Ren <bearice@icybear.net>
2021-12-09 14:07:43 +0800

    2021-q9

--- src/bin/q9.rs
+++ src/bin/q9.rs
@@ -1,43 +1,75 @@
-use std::collections::HashSet;
+use std::{cmp::Reverse, collections::HashSet};
 
 use advent_of_code::common::read_lines;
 
-fn validate(v: &[usize], n: usize) -> bool {
-    //println!("v={:?} n={}", v, n);
-    let r: HashSet<usize> = v
-        .iter()
-        .filter(|x| **x < n && **x != n / 2)
-        .map(|x| n - x)
-        .collect();
-    r.is_disjoint(&v.iter().cloned().collect())
-}
 fn main() {
-    let numbers: Vec<usize> = read_lines("./input9.txt")
-        .map(|x| x.parse().unwrap())
-        .collect();
+    let numbers = read_lines("./input9.txt")
+        .map(|x| {
+            x.into_bytes()
+                .into_iter()
+                .map(|y| (y - 48) as u32)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    // let mut sum = 0;
+    let mut lowest = Vec::new();
+    for (y, row) in numbers.iter().enumerate() {
+        for (x, i) in row.iter().enumerate() {
+            let neighbors = neighbors(x, y, &numbers);
+            if neighbors.iter().all(|&n| n.0 > *i) {
+                lowest.push((*i, x, y));
+            }
+        }
+    }
+    println!("{}", lowest.iter().map(|&n| n.0 + 1).sum::<u32>());
+    let mut basins = Vec::new();
+    for p in lowest {
+        basins.push(find_basin(p, &numbers));
+    }
+    basins.sort_by_key(|x| Reverse(*x));
+    // println!("{:?}", basins);
+    println!("{}", basins[0] * basins[1] * basins[2]);
+}
 
-    let window = 25;
-    let mut n = 0;
-    for i in window..numbers.len() {
-        n = numbers[i];
-        if validate(&numbers[i - window..i], n) {
-            println!("{}", n);
-            break;
+fn find_basin((i, x, y): (u32, usize, usize), numbers: &[Vec<u32>]) -> usize {
+    let mut basin = HashSet::new();
+    let mut visited = HashSet::new();
+    let mut queue = vec![(i, x, y)];
+    while let Some(pos) = queue.pop() {
+        // println!("pos={:?}", pos);
+        if visited.contains(&pos) {
+            continue;
+        }
+        visited.insert(pos);
+        for n in neighbors(pos.1, pos.2, numbers) {
+            // println!("n={:?}", n);
+            if n.0 == 9 || n.0 < pos.0 {
+                continue;
+            }
+            if !visited.contains(&n) {
+                queue.push(n);
+            }
+            basin.insert(n);
         }
     }
-    let (mut a, mut b) = (0, 1);
-    while b < numbers.len() {
-        let s = &numbers[a..=b];
-        let sum = (*s).iter().sum::<usize>();
-        if sum == n {
-            let min = (*s).iter().min().unwrap();
-            let max = (*s).iter().max().unwrap();
-            println!("{}", min + max);
-            break;
-        } else if sum < n {
-            b += 1;
-        } else {
-            a += 1;
-        }
+    // println!("basin={:?}", basin);
+    basin.len() + 1
+}
+
+fn neighbors(x: usize, y: usize, numbers: &[Vec<u32>]) -> Vec<(u32, usize, usize)> {
+    let mut ret = Vec::new();
+    let mut push_number = |x: usize, y: usize| ret.push((numbers[y][x], x, y));
+    if x > 0 {
+        push_number(x - 1, y);
     }
+    if y > 0 {
+        push_number(x, y - 1);
+    }
+    if x < numbers[y].len() - 1 {
+        push_number(x + 1, y);
+    }
+    if y < numbers.len() - 1 {
+        push_number(x, y + 1);
+    }
+    ret
 }
