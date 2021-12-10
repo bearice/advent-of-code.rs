b58d216f7dac99e7dae88c698b779c871b4fc693
Bearice Ren <bearice@icybear.net>
2021-12-10 10:35:12 +0800

    2019-q5

--- src/bin/q5.rs
+++ src/bin/q5.rs
@@ -1,90 +1,106 @@
-use std::collections::HashMap;
-
 use advent_of_code::common::read_lines;
-
-#[derive(Debug, Clone, Copy)]
-struct Line {
-    x1: u32,
-    y1: u32,
-    x2: u32,
-    y2: u32,
-}
-
-impl Line {
-    fn parse(s: String) -> Self {
-        let s = s.replace(" -> ", ",");
-        let mut parts = s.split(',');
-        Line {
-            x1: parts.next().unwrap().parse().unwrap(),
-            y1: parts.next().unwrap().parse().unwrap(),
-            x2: parts.next().unwrap().parse().unwrap(),
-            y2: parts.next().unwrap().parse().unwrap(),
-        }
-    }
-    fn points(&self) -> Vec<(u32, u32)> {
-        let mut points = vec![];
-        if self.x1 == self.x2 {
-            for y in u32::min(self.y1, self.y2)..=u32::max(self.y1, self.y2) {
-                points.push((self.x1, y));
-            }
-        } else if self.y1 == self.y2 {
-            for x in u32::min(self.x1, self.x2)..=u32::max(self.x1, self.x2) {
-                points.push((x, self.y1));
-            }
-        } else {
-            let (x1, x2, y1, y2) = if self.x1 > self.x2 {
-                (self.x2, self.x1, self.y2, self.y1)
-            } else {
-                (self.x1, self.x2, self.y1, self.y2)
-            };
-            for x in x1..=x2 {
-                let d = x - x1;
-                let y = if y1 < y2 { y1 + d } else { y1 - d };
-                points.push((x, y));
-            }
-        }
-        points
-    }
-}
-
 fn main() {
-    let lines = read_lines("./input5.txt")
-        .map(Line::parse)
+    let codes = read_lines("./input5.txt")
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
-
-    let a1 = count_overlaps(
-        lines
-            .iter()
-            .filter(|l| l.x1 == l.x2 || l.y1 == l.y2)
-            .cloned(),
+    println!(
+        "{}",
+        run_program(codes.clone(), [1]).into_iter().last().unwrap()
     );
-    let a2 = count_overlaps(lines.into_iter());
-    println!("a1={}, a2={}", a1, a2);
+    println!("{}", run_program(codes, [5]).into_iter().last().unwrap());
 }
 
-fn count_overlaps(lines: impl Iterator<Item = Line>) -> usize {
-    let mut points = HashMap::new();
-    for line in lines {
-        for point in line.points() {
-            points.insert(point, points.get(&point).unwrap_or(&0) + 1);
+fn read_param(memory: &[i32], param: i32, mode: i32) -> i32 {
+    // println!("param: {} mode:{}", param, mode);
+    match mode {
+        0 => memory[param as usize],
+        1 => param,
+        _ => panic!("Invalid mode"),
+    }
+}
+
+fn run_program(mut codes: Vec<i32>, input: impl IntoIterator<Item = i32>) -> Vec<i32> {
+    let mut input = input.into_iter();
+    let mut output = vec![];
+    let mut pc = 0;
+    while codes[pc] != 99 {
+        let opcode = codes[pc] % 100;
+        let mode = codes[pc] / 100;
+        // println!(
+        //     "pc={} raw={} opcode={} mode={}",
+        //     pc, codes[pc], opcode, mode
+        // );
+        match opcode {
+            1 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                let c = codes[pc + 3];
+                // println!("OP_ADD a={} b={} c={}", a, b, c);
+                codes[c as usize] = a + b;
+                pc += 4;
+            }
+            2 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                let c = codes[pc + 3];
+                // println!("OP_MUL a={} b={} c={}", a, b, c);
+                codes[c as usize] = a * b;
+                pc += 4;
+            }
+            3 => {
+                let a = codes[pc + 1];
+                // println!("OP_IN a={}", a);
+                codes[a as usize] = input.next().unwrap();
+                pc += 2;
+            }
+            4 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                // println!("OP_OUT a={}", a);
+                output.push(a);
+                pc += 2;
+            }
+            5 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                // println!("OP_JNZ a={} b={}", a, b);
+                if a != 0 {
+                    pc = b as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            6 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                // println!("OP_JZ a={} b={}", a, b);
+                if a == 0 {
+                    pc = b as usize;
+                } else {
+                    pc += 3;
+                }
+            }
+            7 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                let c = codes[pc + 3];
+                // println!("OP_LT a={} b={} c={}", a, b, c);
+                codes[c as usize] = if a < b { 1 } else { 0 };
+                pc += 4;
+            }
+            8 => {
+                let a = read_param(&codes, codes[pc + 1], mode % 10);
+                let b = read_param(&codes, codes[pc + 2], mode % 100 / 10);
+                let c = codes[pc + 3];
+                // println!("OP_EQ a={} b={} c={}", a, b, c);
+                codes[c as usize] = if a == b { 1 } else { 0 };
+                pc += 4;
+            }
+            99 => break,
+            _ => panic!("Unknown opcode {}, pc={}", codes[pc], pc),
         }
     }
-    points.values().filter(|&v| *v > 1).count()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse() {
-        let line = Line::parse("1,1,1,3".to_string());
-        assert_eq!(line.x1, 1);
-    }
-
-    #[test]
-    fn test_points() {
-        let line = Line::parse("1,1,1,3".to_string());
-        assert_eq!(line.points(), vec![(1, 1), (1, 2), (1, 3)]);
-    }
+    output
 }
