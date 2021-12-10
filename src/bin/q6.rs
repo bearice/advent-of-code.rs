a8895beae1537b0459dbb9c1e7570f198cbf6c79
Bearice Ren <bearice@icybear.net>
2021-12-10 10:58:24 +0800

    2019-q6

--- src/bin/q6.rs
+++ src/bin/q6.rs
@@ -1,27 +1,36 @@
+use std::collections::HashMap;
+
 use advent_of_code::common::read_lines;
 
 fn main() {
-    let mut fishes = [0u64; 9];
-    read_lines("./input6.txt")
-        .next()
-        .unwrap()
-        .split(',')
-        .for_each(|s| fishes[s.parse::<usize>().unwrap()] += 1);
-
-    for x in 0..256 {
-        next_day(&mut fishes);
-        if x == 80 {
-            println!("day80={}", fishes.iter().sum::<u64>());
-        }
+    let input = read_lines("input6.txt");
+    let mut map = HashMap::new();
+    for line in input {
+        let (parent, node) = line.split_once(')').unwrap();
+        map.insert(node.to_string(), parent.to_string());
     }
-    println!("day256={}", fishes.iter().sum::<u64>());
+    let mut count = 0;
+    for key in map.keys() {
+        count += find_path(&map, key).len();
+    }
+    println!("{}", count);
+
+    let p1 = find_path(&map, "YOU");
+    let p2 = find_path(&map, "SAN");
+    let mut i = 0;
+    while p1[i] == p2[i] {
+        i += 1;
+    }
+    println!("{}", p1.len() + p2.len() - 2 * i);
 }
 
-fn next_day(fishes: &mut [u64; 9]) {
-    let new_fish = fishes[0];
-    for i in 1..=8 {
-        fishes[i - 1] = fishes[i];
+fn find_path<'a>(map: &'a HashMap<String, String>, start: &str) -> Vec<&'a str> {
+    let mut path = Vec::new();
+    let mut current = start;
+    while let Some(parent) = map.get(current) {
+        path.push(parent.as_str());
+        current = parent;
     }
-    fishes[6] += new_fish;
-    fishes[8] = new_fish;
+    path.reverse();
+    path
 }
