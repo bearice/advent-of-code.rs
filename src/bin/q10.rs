81bdcf31a0b6db6f08f8cab5f4a20f043ddaee98
Bearice Ren <bearice@icybear.net>
2021-12-10 14:56:58 +0800

    2021-q10

--- src/bin/q10.rs
+++ src/bin/q10.rs
@@ -1,54 +1,50 @@
 use advent_of_code::common::read_lines;
-use cached::proc_macro::cached;
-
-#[cached]
-fn paths(n: usize) -> usize {
-    match n {
-        0 => 1,
-        1 => 1,
-        2 => 2,
-        3 => 4,
-        x => paths(x - 1) + paths(x - 2) + paths(x - 3),
-    }
-}
 
 fn main() {
-    let mut numbers: Vec<usize> = read_lines("./input10.txt")
-        .map(|x| x.parse().unwrap())
-        .collect();
+    let lines = read_lines("input10.txt");
+    let (s1, s2): (Vec<usize>, Vec<usize>) = lines.map(score_of_line).unzip();
+    let mut s2 = s2.into_iter().filter(|x| *x > 0).collect::<Vec<_>>();
+    s2.sort_unstable();
+    println!("{} {}", s1.into_iter().sum::<usize>(), s2[s2.len() / 2]);
+}
 
-    numbers.push(0);
-    numbers.sort();
-    let mut i = numbers.iter();
-    i.next();
-    let diff: Vec<_> = numbers.iter().zip(i).map(|x| x.1 - x.0).collect();
-    println!("{:?}", diff);
-    let n = diff.iter().fold((0, 1), |mut acc, x| {
-        match *x {
-            1 => acc.0 += 1,
-            3 => acc.1 += 1,
-            _ => (),
-        };
-        acc
-    });
-    println!("{:?}", n.0 * n.1);
-    let mut x = Vec::new();
-    let mut cnt = 0;
-    for i in 0..diff.len() {
-        if diff[i] == 1 {
-            cnt += 1;
-        } else {
-            x.push(cnt);
-            cnt = 0;
+fn score_of_line(line: String) -> (usize, usize) {
+    let mut stack = vec![];
+    for ch in line.chars() {
+        match ch {
+            '(' | '[' | '<' | '{' => stack.push(ch),
+            _ => {
+                let top = stack.pop().unwrap();
+                let x = score_of1(ch);
+                if top != x.0 {
+                    return (x.1, 0);
+                }
+            }
         }
     }
-
-    println!("{}", x.into_iter().map(paths).fold(1, |a, x| a * x));
+    let mut score2 = 0;
+    while !stack.is_empty() {
+        score2 = score2 * 5 + score_of2(stack.pop().unwrap());
+    }
+    (0, score2)
 }
-//(0), 1, 2, 3
 
-// 1=>1
-// 2=>2
-// 3=>4
-// 4=>7
-// 5=>13
+fn score_of1(ch: char) -> (char, usize) {
+    match ch {
+        ')' => ('(', 3),
+        ']' => ('[', 57),
+        '}' => ('{', 1197),
+        '>' => ('<', 25137),
+        _ => panic!("not possible"),
+    }
+}
+
+fn score_of2(ch: char) -> usize {
+    match ch {
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+        _ => panic!("not possible: {}", ch),
+    }
+}
