3b6883e87b6a10eef94e5d371783b6cadcc85bb2
Bearice Ren <bearice@icybear.net>
2021-12-12 14:21:36 +0800

    2021-q12

--- src/bin/q12.rs
+++ src/bin/q12.rs
@@ -1,103 +1,72 @@
+use std::collections::HashMap;
+
 use advent_of_code::common::read_lines;
 
-fn translate1(pos: &mut (isize, isize), dir: &mut isize, cmd: &str, num: isize) {
-    match cmd {
-        "N" => pos.1 += num,
-        "S" => pos.1 -= num,
-        "E" => pos.0 += num,
-        "W" => pos.0 -= num,
-        "F" => translate1(pos, dir, dir_to_cmd(*dir), num),
-        "R" => *dir = (*dir + num) % 360,
-        "L" => *dir = (*dir + 360 - num) % 360,
-        _ => panic!("not possible"),
-    }
-}
-
-fn translate2(pos: &mut (isize, isize), waypt: &mut (isize, isize), cmd: &str, num: isize) {
-    match cmd {
-        "N" => waypt.1 += num,
-        "S" => waypt.1 -= num,
-        "E" => waypt.0 += num,
-        "W" => waypt.0 -= num,
-        "F" => {
-            pos.0 += waypt.0 * num;
-            pos.1 += waypt.1 * num;
-        }
-        "R" => redir_waypt(waypt, num),
-        "L" => redir_waypt(waypt, 360 - num),
-        _ => panic!("not possible"),
-    }
-}
-
-fn dir_to_cmd(dir: isize) -> &'static str {
-    match dir {
-        0 => "N",
-        90 => "E",
-        180 => "S",
-        270 => "W",
-        _ => panic!("not possible"),
-    }
-}
-
-fn redir_waypt(pos: &mut (isize, isize), dir: isize) {
-    match dir {
-        0 => (),
-        90 => *pos = (pos.1, -pos.0),
-        180 => *pos = (-pos.0, -pos.1),
-        270 => *pos = (-pos.1, pos.0),
-        _ => panic!("not possible"),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn translate1() {
-        let mut pos = (0, 0);
-        let mut dir = 0;
-        super::translate1(&mut pos, &mut dir, "F", 10);
-        assert_eq!(pos, (0, 10));
-        assert_eq!(dir, 0);
-        super::translate1(&mut pos, &mut dir, "R", 90);
-        assert_eq!(pos, (0, 10));
-        assert_eq!(dir, 90);
-        super::translate1(&mut pos, &mut dir, "F", 10);
-        assert_eq!(pos, (10, 10));
-        assert_eq!(dir, 90);
-        super::translate1(&mut pos, &mut dir, "L", 180);
-        assert_eq!(pos, (10, 10));
-        assert_eq!(dir, 270);
-        super::translate1(&mut pos, &mut dir, "S", 10);
-        assert_eq!(pos, (10, 0));
-        assert_eq!(dir, 270);
-        super::translate1(&mut pos, &mut dir, "W", 10);
-        assert_eq!(pos, (0, 0));
-        assert_eq!(dir, 270);
-    }
-}
-
-fn part1(lines: Vec<String>) {
-    let mut pos = (0, 0);
-    let mut dir = 90;
-    for line in lines {
-        let (cmd, num) = line.split_at(1);
-        let n = num.parse().unwrap();
-        translate1(&mut pos, &mut dir, cmd, n);
-    }
-    println!("{}", pos.0.abs() + pos.1.abs());
-}
-fn part2(lines: Vec<String>) {
-    let mut pos = (0, 0);
-    let mut waypt = (10, 1);
-    for line in lines {
-        let (cmd, num) = line.split_at(1);
-        let n = num.parse().unwrap();
-        translate2(&mut pos, &mut waypt, cmd, n);
-    }
-    println!("{}", pos.0.abs() + pos.1.abs());
-}
 fn main() {
     let lines: Vec<String> = read_lines("./input12.txt").collect();
-    part1(lines.clone());
-    part2(lines.clone());
+    let mut adjs = HashMap::new();
+    for line in lines {
+        let (a, b) = line.split_once('-').unwrap();
+        adjs.entry(a.to_owned())
+            .or_insert_with(Vec::new)
+            .push(b.to_owned());
+        adjs.entry(b.to_owned())
+            .or_insert_with(Vec::new)
+            .push(a.to_owned());
+    }
+    let paths = path_to_end("start", &adjs, HashMap::new(), filter1);
+    println!("{}", paths.len());
+    let paths = path_to_end("start", &adjs, HashMap::new(), filter2);
+    println!("{}", paths.len());
+}
+
+fn filter1(node: &str, visted: &HashMap<String, u32>) -> bool {
+    let is_small_cave = node.chars().all(char::is_lowercase);
+    let count = visted.get(node).unwrap_or(&0);
+    !(is_small_cave && *count > 0)
+}
+
+fn filter2(node: &str, visited: &HashMap<String, u32>) -> bool {
+    let count = visited.get(node);
+    if node == "start" {
+        count.is_none()
+    } else if node.chars().all(char::is_lowercase) {
+        if count.is_some() {
+            !visited
+                .iter()
+                .any(|c| c.0.chars().all(char::is_lowercase) && *c.1 > 1)
+        } else {
+            true
+        }
+    } else {
+        true
+    }
+}
+
+fn path_to_end<F>(
+    start: &str,
+    adjs: &HashMap<String, Vec<String>>,
+    mut visited: HashMap<String, u32>,
+    filter: F,
+) -> Vec<Vec<String>>
+where
+    F: Fn(&str, &HashMap<String, u32>) -> bool + Copy,
+{
+    if start == "end" {
+        return vec![vec![start.to_owned()]];
+    }
+    let mut paths = Vec::new();
+    if !filter(start, &visited) {
+        return vec![];
+    }
+    let count = visited.entry(start.to_owned()).or_insert(0);
+    *count += 1;
+    for adj in adjs.get(start).unwrap() {
+        let new_paths = path_to_end(adj, adjs, visited.clone(), filter);
+        for mut path in new_paths {
+            path.push(start.to_owned());
+            paths.push(path);
+        }
+    }
+    paths
 }
