6c558574c5f8dd4d7c46064e14881409c79b1f74
Bearice Ren <bearice@icybear.net>
2021-12-15 18:56:18 +0800

    2021-q15

--- src/bin/q15.rs
+++ src/bin/q15.rs
@@ -1,51 +1,81 @@
-use std::collections::HashMap;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
-#[derive(Debug)]
-struct Day15 {
-    numbers: HashMap<usize, (usize, usize)>,
-    last: usize,
-    init: Vec<usize>,
-    n: usize,
-}
+use advent_of_code::common::read_u8_matrix;
 
-impl Day15 {
-    fn new(init: Vec<usize>) -> Self {
-        Self {
-            numbers: HashMap::new(),
-            last: 0,
-            n: 0,
-            init,
-        }
-    }
-}
-
-impl Iterator for Day15 {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let n = self.n + 1;
-        self.last = if self.n < self.init.len() {
-            self.init[self.n]
-        } else {
-            if let Some(x) = &self.numbers.get(&self.last) {
-                x.1 - x.0
-            } else {
-                0
-            }
-        };
-        self.n = n;
-        if let Some(x) = self.numbers.get_mut(&self.last) {
-            x.0 = x.1;
-            x.1 = n;
-        // println!("{} {:?}", n, x)
-        } else {
-            self.numbers.insert(self.last, (n, n));
-        }
-        Some(self.last)
-    }
-}
 fn main() {
-    let i = vec![0, 13, 1, 16, 6, 17];
-    let mut it = Day15::new(i);
-    println!("{}", it.nth(30000000 - 1).unwrap());
+    let map = read_u8_matrix("input15.txt");
+    println!(
+        "{:?}",
+        shortest_path(&map, (0, 0), (map.len() - 1, map[0].len() - 1))
+    );
+    // println!("{:?}", enlarge(vec![vec![8]]))
+    let map = enlarge(map);
+    println!(
+        "{:?}",
+        shortest_path(&map, (0, 0), (map.len() - 1, map[0].len() - 1))
+    );
+}
+
+fn enlarge(map: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
+    let mut ret = Vec::new();
+    for x in 0..5 {
+        for line in &map {
+            let mut new_line = Vec::new();
+            for y in 0..5 {
+                for &n in line {
+                    let n = n as u32 + x + y - 1;
+                    new_line.push((n % 9) as u8 + 1);
+                }
+            }
+            ret.push(new_line);
+        }
+    }
+    ret
+}
+
+// Dijkstra’s algorithm from rust std library documentation
+fn shortest_path(nodes: &[Vec<u8>], start: (usize, usize), goal: (usize, usize)) -> Option<usize> {
+    let max = (nodes.len(), nodes[0].len());
+    let mut dist: Vec<Vec<usize>> = (0..nodes.len())
+        .map(|_| (0..nodes[0].len()).map(|_| usize::MAX).collect())
+        .collect();
+
+    let mut heap = BinaryHeap::new();
+
+    dist[start.0][start.1] = 0;
+    heap.push(Reverse((0, start)));
+
+    while let Some(Reverse((cost, pos))) = heap.pop() {
+        if pos == goal {
+            return Some(cost);
+        }
+        if cost > dist[pos.0][pos.1] {
+            continue;
+        }
+        for edge in adj_list(pos, max) {
+            let next = (cost + nodes[edge.0][edge.1] as usize, edge);
+            if next.0 < dist[edge.0][edge.1] {
+                heap.push(Reverse(next));
+                dist[edge.0][edge.1] = next.0;
+            }
+        }
+    }
+    None
+}
+
+fn adj_list(pos: (usize, usize), max: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut adj = vec![];
+    if pos.0 > 0 {
+        adj.push((pos.0 - 1, pos.1));
+    }
+    if pos.0 < max.0 - 1 {
+        adj.push((pos.0 + 1, pos.1));
+    }
+    if pos.1 > 0 {
+        adj.push((pos.0, pos.1 - 1));
+    }
+    if pos.1 < max.1 - 1 {
+        adj.push((pos.0, pos.1 + 1));
+    }
+    adj
 }
