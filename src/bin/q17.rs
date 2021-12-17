1eb2f74e64d18bdb1607998595dc1450e4c186e8
Bearice Ren <bearice@icybear.net>
2021-12-17 21:11:21 +0800

    2021-q17

--- src/bin/q17.rs
+++ src/bin/q17.rs
@@ -1,94 +1,65 @@
-use advent_of_code::common::read_lines;
-
+use itertools::Itertools;
+use std::cmp::Ordering;
 fn main() {
-    let mut cubes = init();
-    println!("{:?}", cubes);
-    for i in 0..6 {
-        cubes = cycle(cubes);
-        println!("c={} {}", i, cubes.len());
-    }
-}
-fn init() -> Vec<CubePos> {
-    let mut ret = vec![];
-    let lines = read_lines("./input17.txt");
-    let mut y = 0;
-    let mut x = 0;
-    for line in lines {
-        for ch in line.chars() {
-            if ch == '#' {
-                ret.push((x, y, 0, 0));
-            }
-            x += 1
-        }
-        x = 0;
-        y += 1;
-    }
-    ret
-}
-use std::collections::HashMap;
-fn cycle(cubes: Vec<CubePos>) -> Vec<CubePos> {
-    let neighbors: Vec<CubePos> = cubes.iter().flat_map(|c| neighbors(c)).collect();
-    let mut cube_map = HashMap::new();
-    for c in cubes.into_iter() {
-        cube_map.insert(c, (true, 0));
-    }
-    for p in neighbors {
-        if let Some(c) = cube_map.get_mut(&p) {
-            c.1 += 1;
-        } else {
-            cube_map.insert(p, (false, 1));
-        }
-    }
-    let mut ret = vec![];
-
-    for (k, mut c) in cube_map.into_iter() {
-        if c.0 {
-            c.0 = c.1 == 2 || c.1 == 3;
-        } else {
-            c.0 = c.1 == 3;
-        }
-        // println!("{:?} {:?}", k, c);
-        if c.0 {
-            ret.push(k);
-        }
-    }
-    // ret.sort_by_key(|x| (x.1, x.0));
-    // print_cube(ret.iter());
-    ret
-}
-
-// fn print_cube<'a, I>(v: I)
-// where
-//     I: Iterator<Item = &'a CubePos>,
-// {
-//     let t: Vec<CubePos> = v.filter(|x| x.2 == 0).map(|x| *x).collect();
-//     println!("{:?}", t);
-//     // let min_x = t.iter().map(|x| x.pos.0).min().unwrap();
-//     // let min_y = t.iter().map(|x| x.pos.1).min().unwrap();
-// }
-type CubePos = (i32, i32, i32, i32);
-fn neighbors(p: &CubePos) -> Vec<CubePos> {
-    let mut ret = vec![];
-    for x in p.0 - 1..p.0 + 2 {
-        for y in p.1 - 1..p.1 + 2 {
-            for z in p.2 - 1..p.2 + 2 {
-                for w in p.3 - 1..p.3 + 2 {
-                    if (x, y, z, w) != *p {
-                        ret.push((x, y, z, w));
-                    }
-                }
+    // let target = (20, 30, -10, -5);
+    let target = (156, 202, -110, -69);
+    let x_rng = find_x_range(target.0, target.1);
+    // println!("x_rng: {:?}", x_rng);
+    let y_rng = find_y_range(target.2, target.3);
+    // println!("y_rng: {:?}", y_rng);
+    let mut common = vec![];
+    for &x in x_rng.iter() {
+        for &y in y_rng.iter() {
+            if x.0 == y.0 || (x.2 == 0 && y.0 > x.0) {
+                common.push((x.0, x.1, y.1, y.2));
             }
         }
     }
+    println!("max: {}", common.iter().map(|&x| x.3).max().unwrap());
+    let uniq = common.iter().map(|&x| (x.1, x.2)).unique();
+    println!("uniq: {}", uniq.count());
+}
+
+fn find_x_range(start: i32, end: i32) -> Vec<(i32, i32, i32)> {
+    let mut ret = vec![];
+    for v in 1..=end {
+        let mut x = 0;
+        let mut vx = v;
+        let mut i = 0;
+        while x <= end && vx != 0 {
+            i += 1;
+            x += vx;
+            vx += match vx.cmp(&0) {
+                Ordering::Less => 1,
+                Ordering::Greater => -1,
+                Ordering::Equal => 0,
+            };
+            if x >= start && x <= end {
+                ret.push((i, v, vx));
+            }
+        }
+    }
+    // ret.sort_unstable();
     ret
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn neighbors() {
-        let n = super::neighbors(&(0, 0, 0, 0));
-        assert!(n.len() == 80);
-        println!("{:?}", n)
+fn find_y_range(start: i32, end: i32) -> Vec<(i32, i32, i32)> {
+    let mut ret = vec![];
+    for v in start..=start.abs() {
+        let mut y = 0;
+        let mut vy = v;
+        let mut i = 0;
+        let mut max = 0;
+        while y >= start {
+            i += 1;
+            y += vy;
+            vy -= 1;
+            max = max.max(y);
+            if y >= start && y <= end {
+                ret.push((i, v, max));
+            }
+        }
     }
+    // ret.sort_unstable();
+    ret
 }
