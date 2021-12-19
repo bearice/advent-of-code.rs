d886538631eb5cb6ecf623ab4ddb6d2e72433700
Bearice Ren <bearice@icybear.net>
2021-12-19 19:23:51 +0800

    2019-q11

--- src/bin/q11.rs
+++ src/bin/q11.rs
@@ -1,137 +1,228 @@
+use std::collections::HashMap;
+
 use advent_of_code::common::read_lines;
 
-type Seats = Vec<Vec<char>>;
-type SeatAdj = Vec<Vec<Vec<(usize, usize)>>>;
-
-fn get_seats<'a>(seats: &'a Seats, (x, y): (usize, usize)) -> Option<&'a char> {
-    seats.get(y).and_then(|l| l.get(x))
-}
-
-fn is_occupied(x: &&char) -> bool {
-    *x == &'#'
-}
-
-fn round(seats: &mut Seats, adjs: &SeatAdj, threshold: usize) -> usize {
-    let mut ret = 0;
-    let snapshot = seats.clone();
-    // println!("Round Begin");
-
-    for y in 0..seats.len() {
-        for x in 0..seats[y].len() {
-            if let Some(s) = get_seats(&snapshot, (x, y)) {
-                let adjs = adjs[y][x]
-                    .iter()
-                    .map(|p| {
-                        let ret = get_seats(&snapshot, *p);
-                        // if x == 0 && y == 1 {
-                        //     println!("p={:?} ret={:?}", p, ret);
-                        // }
-                        ret
-                    })
-                    .filter_map(|x| x);
-                let cnt = adjs.filter(is_occupied).count();
-                //print!("x={} y={} cnt={} ", x, y, cnt);
-                match *s {
-                    'L' => {
-                        if cnt == 0 {
-                            seats[y][x] = '#';
-                            ret += 1;
-                        }
-                    }
-                    '#' => {
-                        if cnt >= threshold {
-                            seats[y][x] = 'L';
-                            ret += 1;
-                        }
-                    }
-                    _ => (),
-                }
-                // print!("{}{}{}|", s, cnt, seats[y][x]);
-            }
-        }
-        // println!("");
-    }
-    ret
-}
-
-fn part1_adj(_: &Seats, (x, y): (usize, usize)) -> Vec<(usize, usize)> {
-    let mut ret = vec![(x, y + 1), (x + 1, y), (x + 1, y + 1)];
-    if x > 0 {
-        ret.push((x - 1, y));
-        ret.push((x - 1, y + 1));
-    }
-    if y > 0 {
-        ret.push((x, y - 1));
-        ret.push((x + 1, y - 1));
-    }
-    if x > 0 && y > 0 {
-        ret.push((x - 1, y - 1))
-    }
-    ret
-}
-fn part2_adj(seats: &Seats, (x, y): (usize, usize)) -> Vec<(usize, usize)> {
-    fn try_dir(seats: &Seats, x: usize, y: usize, dx: isize, dy: isize) -> Option<(usize, usize)> {
-        let (tx, ty) = (x as isize + dx, y as isize + dy);
-        if tx < 0 || ty < 0 {
-            None
-        } else if let Some(ch) = get_seats(seats, (tx as usize, ty as usize)) {
-            // println!("dx={} dy={} tx={} ty={} ch={}", dx, dy, tx + 1, ty + 1, ch);
-            if *ch == 'L' {
-                // println!("ret={},{}", tx, ty);
-                Some((tx as usize, ty as usize))
-            } else {
-                try_dir(seats, tx as usize, ty as usize, dx, dy)
-            }
-        } else {
-            None
-        }
-    }
-    let ret = vec![
-        try_dir(seats, x, y, -1, -1),
-        try_dir(seats, x, y, -1, 0),
-        try_dir(seats, x, y, -1, 1),
-        try_dir(seats, x, y, 0, -1),
-        try_dir(seats, x, y, 0, 1),
-        try_dir(seats, x, y, 1, -1),
-        try_dir(seats, x, y, 1, 0),
-        try_dir(seats, x, y, 1, 1),
-    ];
-
-    ret.into_iter().filter_map(|x| x).collect()
-}
-
-fn run_rounds<F>(mut seats: Seats, adj_fn: F, threshold: usize) -> usize
-where
-    F: Fn(&Seats, (usize, usize)) -> Vec<(usize, usize)>,
-{
-    let mut adjs = Vec::with_capacity(seats.len());
-    for y in 0..seats.len() {
-        let mut row = Vec::with_capacity(seats[y].len());
-        for x in 0..seats[y].len() {
-            let cell = adj_fn(&seats, (x, y));
-            row.push(cell);
-        }
-        adjs.push(row);
-    }
-
-    println!("ajd[1][9]={:?}", adjs[1][9]);
-    let mut l = 0;
-    let mut n = round(&mut seats, &adjs, threshold);
-    while l != n {
-        l = n;
-        n = round(&mut seats, &adjs, threshold);
-    }
-    seats
-        .into_iter()
-        .map(|l| l.iter().filter(is_occupied).count())
-        .sum()
-}
 fn main() {
-    let seats: Seats = read_lines("./input11.txt")
-        .map(|s| s.chars().collect())
-        .collect();
+    let codes = read_lines("./input11.txt")
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    let program = Program::new(codes);
+    let mut panels = HashMap::new();
+    run_robot(&mut panels, program.clone());
+    println!("{}", panels.len());
 
-    // println!("{:?}", part2_adj(&seats, (3, 4)));
-    println!("p1={}", run_rounds(seats.clone(), part1_adj, 4));
-    println!("p2={}", run_rounds(seats.clone(), part2_adj, 5));
+    let mut panels = HashMap::new();
+    panels.insert((0, 0), 1);
+    run_robot(&mut panels, program);
+    draw_points(panels);
+}
+
+fn draw_points(points: HashMap<(i32, i32), i64>) {
+    let (min_x, max_x, min_y, max_y) = find_edge(points.keys().cloned());
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            if points.get(&(x, y)).unwrap_or(&0) == &1 {
+                print!("#");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
+
+fn find_edge(points: impl IntoIterator<Item = (i32, i32)>) -> (i32, i32, i32, i32) {
+    points
+        .into_iter()
+        .fold((i32::MAX, i32::MIN, i32::MAX, i32::MIN), |acc, x| {
+            (
+                std::cmp::min(acc.0, x.0),
+                std::cmp::max(acc.1, x.0),
+                std::cmp::min(acc.2, x.1),
+                std::cmp::max(acc.3, x.1),
+            )
+        })
+}
+
+fn run_robot(panels: &mut HashMap<(i32, i32), i64>, mut program: Program) {
+    let mut dir = 0;
+    let mut pos = (0, 0);
+    loop {
+        let color = panels.entry(pos).or_insert(0);
+        let new_color = program.run_program(Some(*color));
+        let new_color = if let ProgramOutput::Output(new_color) = new_color {
+            new_color
+        } else {
+            // println!("{:?}", new_color);
+            break;
+        };
+
+        let new_dir = if let ProgramOutput::Output(new_dir) = program.run_program(None) {
+            new_dir
+        } else {
+            panic!("unexpected output");
+        };
+        *color = new_color;
+        dir += if new_dir == 0 { 3 } else { 1 };
+        dir %= 4;
+        match dir {
+            0 => pos.1 -= 1,
+            1 => pos.0 += 1,
+            2 => pos.1 += 1,
+            3 => pos.0 -= 1,
+            _ => panic!("Invalid direction"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum ProgramOutput {
+    Halt,
+    NeedInput,
+    Output(i64),
+}
+
+#[derive(Clone)]
+struct Program {
+    codes: Vec<i64>,
+    pc: usize,
+    base: i64,
+}
+
+impl Program {
+    fn new(mut codes: Vec<i64>) -> Program {
+        codes.extend([0; 1000]);
+        Program {
+            codes,
+            pc: 0,
+            base: 0,
+        }
+    }
+
+    fn run_program(&mut self, mut input: Option<i64>) -> ProgramOutput {
+        fn read_param(memory: &[i64], param: i64, mode: i64, base: i64) -> i64 {
+            // println!("param: {} mode:{}", param, mode);
+            match mode {
+                0 => memory[param as usize],
+                1 => param,
+                2 => memory[(base + param) as usize],
+                _ => panic!("Invalid mode"),
+            }
+        }
+        fn write_param(memory: &mut [i64], param: i64, mode: i64, base: i64, value: i64) {
+            // println!("param: {} mode:{}", param, mode);
+            match mode {
+                0 => memory[param as usize] = value,
+                2 => memory[(base + param) as usize] = value,
+                _ => panic!("Invalid mode"),
+            }
+        }
+        let codes = &mut self.codes;
+        let mut pc = self.pc;
+        while codes[pc] != 99 {
+            let opcode = codes[pc] % 100;
+            let mode = codes[pc] / 100;
+            // println!(
+            //     "pc={} raw={} opcode={} mode={} base={}",
+            //     pc, codes[pc], opcode, mode, self.base
+            // );
+            match opcode {
+                1 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    let c = codes[pc + 3];
+                    // println!("OP_ADD a={} b={} c={}", a, b, c);
+                    write_param(codes, c, mode % 1000 / 100, self.base, a + b);
+                    pc += 4;
+                }
+                2 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    let c = codes[pc + 3];
+                    // println!("OP_MUL a={} b={} c={}", a, b, c);
+                    write_param(codes, c, mode % 1000 / 100, self.base, a * b);
+                    pc += 4;
+                }
+                3 => {
+                    let a = codes[pc + 1];
+                    // println!("OP_IN a={}", a);
+                    if let Some(i) = input {
+                        write_param(codes, a, mode % 10, self.base, i);
+                        input = None;
+                    } else {
+                        return ProgramOutput::NeedInput;
+                    }
+                    pc += 2;
+                }
+                4 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    // println!("OP_OUT a={}", a);
+                    pc += 2;
+                    self.pc = pc;
+                    return ProgramOutput::Output(a);
+                }
+                5 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    // println!("OP_JNZ a={} b={}", a, b);
+                    if a != 0 {
+                        pc = b as usize;
+                    } else {
+                        pc += 3;
+                    }
+                }
+                6 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    // println!("OP_JZ a={} b={}", a, b);
+                    if a == 0 {
+                        pc = b as usize;
+                    } else {
+                        pc += 3;
+                    }
+                }
+                7 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    let c = codes[pc + 3];
+                    // println!("OP_LT a={} b={} c={}", a, b, c);
+                    write_param(
+                        codes,
+                        c,
+                        mode % 1000 / 100,
+                        self.base,
+                        if a < b { 1 } else { 0 },
+                    );
+
+                    pc += 4;
+                }
+                8 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    let b = read_param(codes, codes[pc + 2], mode % 100 / 10, self.base);
+                    let c = codes[pc + 3];
+                    // println!("OP_EQ a={} b={} c={}", a, b, c);
+                    write_param(
+                        codes,
+                        c,
+                        mode % 1000 / 100,
+                        self.base,
+                        if a == b { 1 } else { 0 },
+                    );
+                    pc += 4;
+                }
+                9 => {
+                    let a = read_param(codes, codes[pc + 1], mode % 10, self.base);
+                    // println!("OP_BASE a={}", a);
+                    self.base += a;
+                    pc += 2;
+                }
+                99 => break,
+                _ => panic!("Unknown opcode {}, pc={}", codes[pc], pc),
+            }
+        }
+        ProgramOutput::Halt
+    }
 }
