ba118ae79e8709065ac0fcdebfd02324b33f5ea8
Bearice Ren <bearice@icybear.net>
2021-12-27 16:02:33 +0800

    2019-q19

--- src/bin/q19.rs
+++ src/bin/q19.rs
@@ -1,61 +1,79 @@
-use advent_of_code::common::ReadChunks;
-use regex::Regex;
+use std::collections::HashMap;
 
-fn gen_regex(v: &Vec<(usize, &str)>, s: &str) -> String {
-    let l = s.split_ascii_whitespace();
-    let r: String = l
-        .map(|i| {
-            if let Ok(n) = i.parse::<usize>() {
-                let x = v[n].1;
-                gen_regex(v, x)
-            } else {
-                i.replace("\"", "").to_owned()
-            }
-        })
-        .collect();
-    if r.contains("|") {
-        format!("({})", r)
-    } else {
-        r
-    }
-}
+use advent_of_code::common::{Program, ProgramOutput};
+
 fn main() {
-    let mut chunks = ReadChunks::new("./input19.txt");
-    let lines = chunks.next().unwrap();
-    let mut l: Vec<_> = lines
-        .iter()
-        .map(|x| {
-            let mut i = x.split(":");
-            let idx: usize = i.next().unwrap().parse().unwrap();
-            let cnt = i.next().unwrap();
-            (idx, cnt)
-        })
-        .collect();
-    l.sort();
+    let code = Program::from_file("input19.txt");
+    println!("{}", solve1(&code));
+    println!("{}", solve2(&code));
+}
 
-    let mut r = gen_regex(&l, l[0].1);
-    r = format!("^{}$", r);
-    let regex = Regex::new(&r).unwrap();
-    let lines = chunks.next().unwrap();
-    let p1: Vec<_> = lines
-        .iter()
-        .map(|s| regex.is_match(&s))
-        .filter(|x| *x)
-        .collect();
-    println!("p1={}", p1.len());
+fn solve1(code: &Program) -> i64 {
+    let mut i = 0;
+    for x in 0..50 {
+        for y in 0..50 {
+            if check_pos(code, x, y) > 0 {
+                i += 1;
+            }
+        }
+    }
+    i
+}
 
-    //0: 8 11 -> 42+ (?<pn> 42 (?&pn) 31)
-    //8: 42 | 42 8
-    //11: 42 31 | 42 11 31
-    // let r42 = gen_regex(&l, l[42].1);
-    // let r31 = gen_regex(&l, l[31].1);
-    // let r2s = format!("^{}+(?<pn>{}(?&pn)?{})$", r42, r42, r31);
-    // let mut r2 = pcre::Pcre::compile(&r2s).unwrap();
+fn solve2(code: &Program) -> i64 {
+    let delta = 99;
+    let mut cache = HashMap::new();
+    let mut y1 = 1000;
+    let mut y2 = y1 + delta;
+    let mut x1 = find_edge(code, &mut cache, y1).1;
+    let mut x2 = find_edge(code, &mut cache, y2).0;
 
-    // let p2: Vec<_> = lines
-    //     .iter()
-    //     .map(|s| r2.exec(&s))
-    //     .filter(Option::is_some)
-    //     .collect();
-    // println!("p2={}", p2.len());
+    loop {
+        println!("x1: {:?} x2: {:?}  x1-x2:{}", x1, x2, x1 - x2);
+        if x1 - x2 >= delta {
+            break;
+        }
+        y1 += 1;
+        y2 += 1;
+        x1 = find_edge(code, &mut cache, y1).1;
+        x2 = find_edge(code, &mut cache, y2).0;
+    }
+    println!("chk({},{})={}", x1, y1, check_pos(code, x1, y1));
+    println!("chk({},{})={}", x2, y2, check_pos(code, x2, y2));
+    x2 * 10000 + y1
+}
+
+fn find_edge(code: &Program, cache: &mut HashMap<i64, (i64, i64)>, y: i64) -> (i64, i64) {
+    if cache.contains_key(&y) {
+        return cache[&y];
+    }
+    let mut x = if let Some(e) = cache.get(&(y - 1)) {
+        e.1 + 10
+    } else {
+        y
+    };
+    while check_pos(code, x, y) == 0 {
+        x -= 1;
+    }
+    let max = x;
+    while check_pos(code, x, y) == 1 {
+        x -= 1;
+    }
+    let min = x + 1;
+    let ret = (min, max);
+    cache.insert(y, ret);
+    ret
+}
+
+fn check_pos(code: &Program, x: i64, y: i64) -> i64 {
+    let mut code = code.clone();
+    let out = code.run_program(Some(x));
+    if out != ProgramOutput::NeedInput {
+        panic!("unexpected output");
+    }
+    let out = code.run_program(Some(y));
+    if let ProgramOutput::Output(out) = out {
+        return out;
+    }
+    panic!("unexpected output");
 }
