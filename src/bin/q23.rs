990912b18627e5422ee7491f1b9459032dfd6392
Bearice Ren <bearice@icybear.net>
2021-12-29 21:15:53 +0800

    2021-q23

--- src/bin/q23.rs
+++ src/bin/q23.rs
@@ -1,68 +1,64 @@
-fn step(v: &mut Vec<usize>) {
-    let mut max = v.len() - 1;
-    let cur = v[0];
-    // println!("cur={}", cur);
-    let mut dst = cur - 1;
-    let mut take = vec![0, 0, 0];
-    let mut next = v[cur];
-    for t in 0..3 {
-        take[t] = next;
-        next = v[next];
-    }
-    v[cur] = next;
-    // rem -= 4;
-    while take.contains(&max) {
-        max -= 1;
-    }
-    // j %= l;
-    // println!("take={:?}", take);
-    while take.contains(&dst) {
-        dst -= 1;
-        // println!("dst candidate={}", dst);
-    }
-    if dst == 0 {
-        dst = max;
-    }
-    // println!("dst={}", dst);
-    next = dst;
-    for t in take {
-        let old = v[next];
-        v[next] = t;
-        v[t] = old;
-        next = t;
-    }
-    v[0] = v[cur];
-}
+use std::collections::VecDeque;
 
-fn dump(temp: &Vec<usize>) {
-    let mut hd = temp[0];
-    while temp[hd] != temp[0] {
-        print!("{}", hd);
-        hd = temp[hd];
-    }
-    println!("{}", hd);
-}
+use advent_of_code::common::{Program, ProgramOutput};
+use itertools::Itertools;
+
 fn main() {
-    let mut initial = vec![3, 2, 6, 5, 1, 9, 4, 7, 8];
-    initial.append(&mut (10..=1_000_000).collect::<Vec<_>>());
-    println!("total={}", initial.len());
-    let mut pos = 0;
-    let mut temp = initial.clone();
-    temp.push(0);
-    for i in initial {
-        temp[pos] = i;
-        pos = i;
+    let code = Program::from_file("input23.txt");
+    let mut rx = (0..50).map(|x| VecDeque::from([x])).collect_vec();
+    let mut tx = (0..50).map(|_| VecDeque::new()).collect_vec();
+    let mut mem = (0..50).map(|_| code.clone()).collect_vec();
+    let mut first = None;
+    let mut nat = None;
+    let mut last = None;
+    loop {
+        for buf in tx.iter_mut() {
+            // println!("tx[{}]: {:?}", i, buf);
+            while buf.len() >= 3 {
+                let dst = buf.pop_front().unwrap() as usize;
+                let x = buf.pop_front().unwrap();
+                let y = buf.pop_front().unwrap();
+                if dst == 255 {
+                    if first.is_none() {
+                        first = Some((x, y));
+                    }
+                    nat = Some((x, y));
+                } else {
+                    rx[dst].push_back(x);
+                    rx[dst].push_back(y);
+                }
+            }
+        }
+        let mut has_io = false;
+        for (i, code) in mem.iter_mut().enumerate() {
+            while let ProgramOutput::Output(x) = run_with_inputs(code, || {
+                has_io |= !rx[i].is_empty();
+                rx[i].pop_front().or(Some(-1))
+            }) {
+                has_io |= true;
+                tx[i].push_back(x);
+            }
+        }
+        if !has_io {
+            let (x, y) = nat.unwrap();
+            rx[0].push_back(x);
+            rx[0].push_back(y);
+            println!("sent nat pkt: {} {} {}", 0, x, y);
+            if last.is_some() && last.unwrap() == (x, y) {
+                break;
+            } else {
+                last = Some((x, y));
+            }
+        }
     }
-    temp[pos] = temp[0];
+    println!("first: {:?}", first);
+    println!("last: {:?}", last);
+}
 
-    // println!("temp={}", temp.len());
-    // dump(&temp);
-    for _i in 0usize..10_000_000 {
-        step(&mut temp);
-        // dump(&temp);
-    }
-    // println!("{:?}", temp);
-    let n1 = temp[1];
-    let n2 = temp[n1];
-    println!("{} {} {}", n1, n2, n1 * n2);
+pub fn run_with_inputs(code: &mut Program, inputs: impl FnOnce() -> Option<i64>) -> ProgramOutput {
+    let input = match code.run_program(None) {
+        ProgramOutput::NeedInput => inputs(),
+        x => return x,
+    };
+    code.run_program(input)
 }
