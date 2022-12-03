b7f5dc907679bf60c8d073ae5ff1886836d016a1
Bearice Ren <bearice@icybear.net>
2022-12-03 18:19:25 +0800

    2022-q1

--- src/bin/q1.rs
+++ src/bin/q1.rs
@@ -1,28 +1,13 @@
-use advent_of_code::common::read_lines;
+use advent_of_code::common::ReadChunks;
+use itertools::Itertools;
+use std::cmp::Reverse;
 fn main() {
-    let v: Vec<i32> = read_lines("./input1.txt")
-        .map(|x| x.parse().unwrap())
+    let v: Vec<i32> = ReadChunks::new("./input1.txt")
+        .map(|x| x.into_iter().map(|s| s.parse::<i32>().unwrap()).sum())
         .collect();
-
-    fn count_incr(v: impl Iterator<Item = i32>) -> i32 {
-        v.fold((-1, 0), |(n, last), x| {
-            // println!("n={} last={} x={}", n, last, x);
-            if n == -1 {
-                (0, x)
-            } else if x > last {
-                (n + 1, x)
-            } else {
-                (n, x)
-            }
-        })
-        .0
-    }
-    let a1 = count_incr(v.iter().copied());
-    println!("a1={}", a1);
-    let v1 = v.iter();
-    let v2 = v.iter().zip(v1.skip(1));
-    let v3 = v.iter().zip(v2.skip(1));
-    let vs = v3.map(|(x, (y, z))| x + y + z);
-    let a2 = count_incr(vs);
-    println!("a2={}", a2);
+    println!("max: {}", v.iter().max().unwrap());
+    println!(
+        "top3: {}",
+        v.iter().sorted_by_key(|x| Reverse(*x)).take(3).sum::<i32>()
+    );
 }
