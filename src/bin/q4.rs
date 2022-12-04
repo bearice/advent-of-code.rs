bfa14d4a0af33e97ed2d1e9d6eda1512535478a8
Bearice Ren <bearice@icybear.net>
2022-12-04 14:02:32 +0800

    2022-q4

--- src/bin/q4.rs
+++ src/bin/q4.rs
@@ -1,62 +1,25 @@
-use advent_of_code::common::ReadChunks;
+use advent_of_code::common::read_lines;
+use itertools::Itertools;
 
 fn main() {
-    let mut chunks = ReadChunks::new("./input4.txt");
-    let cards = chunks.next().unwrap();
-    let cards = cards[0]
-        .split(',')
-        .map(|x| x.parse().unwrap())
-        .collect::<Vec<u8>>();
-    let mut boards = chunks
-        .map(|lines| {
-            (
-                lines
-                    .into_iter()
-                    .map(|s| {
-                        s.split_ascii_whitespace()
-                            .map(|v| (v.parse().unwrap(), false))
-                            .collect::<Vec<(u8, bool)>>()
-                    })
-                    .collect::<Vec<_>>(),
-                false,
-            )
-        })
-        .collect::<Vec<_>>();
-    let mut score = vec![];
-    for n in cards {
-        for (board, won) in boards.iter_mut() {
-            if *won {
-                continue;
-            }
-            'next: for x in 0..5 {
-                for y in 0..5 {
-                    if board[x][y].0 == n {
-                        board[x][y].1 = true;
-                        if check_board(board, x, y) {
-                            score.push(score_board(board, n));
-                            *won = true;
-                        }
-                        break 'next;
-                    }
-                }
-            }
-        }
-    }
-    println!("first: {} last: {}", score[0], score[score.len() - 1]);
+    let parts = read_lines("./input4.txt").map(parse_line).collect_vec();
+    let a1 = parts.iter().filter(contains).count();
+    println!("{}", a1);
+    let a2 = parts.iter().filter(overlap).count();
+    println!("{}", a2);
 }
 
-fn check_board(board: &[Vec<(u8, bool)>], x: usize, y: usize) -> bool {
-    board[x].iter().all(|(_, r)| *r) || board.iter().map(|b| b[y]).all(|(_, r)| r)
+fn parse_line(line: String) -> ((i32, i32), (i32, i32)) {
+    let mut parts = line.split([',', '-']).map(str::parse).map(Result::unwrap);
+    let a = parts.next_tuple().unwrap();
+    let b = parts.next_tuple().unwrap();
+    (a, b)
 }
 
-fn score_board(board: &[Vec<(u8, bool)>], n: u8) -> u32 {
-    let mut ret = 0;
-    for line in board {
-        for cell in line {
-            if !cell.1 {
-                ret += cell.0 as u32;
-            }
-        }
-    }
-    ret * n as u32
+fn contains(((a, b), (c, d)): &&((i32, i32), (i32, i32))) -> bool {
+    (a <= c && b >= d) || (a >= c && b <= d)
+}
+
+fn overlap(((a, b), (c, d)): &&((i32, i32), (i32, i32))) -> bool {
+    a <= d && c <= b
 }
