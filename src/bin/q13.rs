0601b33b2ac3deb47bb63649391c164c274e534a
Bearice Ren <bearice@icybear.net>
2022-12-13 17:27:20 +0800

    2022-q13

--- src/bin/q13.rs
+++ src/bin/q13.rs
@@ -1,66 +1,92 @@
-use std::collections::HashSet;
+use std::cmp::Ordering;
 
 use advent_of_code::common::ReadChunks;
+use json::JsonValue;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Expr {
+    List(Vec<Expr>),
+    Value(i32),
+}
+
+impl Expr {
+    fn parse(input: &str) -> Expr {
+        json::parse(input).unwrap().into()
+    }
+    fn is_value(&self) -> bool {
+        matches!(self, Expr::Value(_))
+    }
+    fn as_value(&self) -> i32 {
+        if let Expr::Value(v) = self {
+            *v
+        } else {
+            unreachable!()
+        }
+    }
+    fn as_list(&self) -> Vec<&Expr> {
+        match self {
+            Self::List(l) => l.iter().collect(),
+            Self::Value(_) => vec![self],
+        }
+    }
+}
+
+impl Ord for Expr {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.is_value() && other.is_value() {
+            self.as_value().cmp(&other.as_value())
+        } else {
+            let left = self.as_list();
+            let right = other.as_list();
+            let len = (left.len(), right.len());
+            for (l, r) in left.into_iter().zip(right) {
+                let ret = l.cmp(r);
+                if ret == Ordering::Equal {
+                    continue;
+                } else {
+                    return ret;
+                }
+            }
+            len.0.cmp(&len.1)
+        }
+    }
+}
+
+impl PartialOrd for Expr {
+    fn partial_cmp(&self, other: &Expr) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl From<JsonValue> for Expr {
+    fn from(input: JsonValue) -> Self {
+        match input {
+            JsonValue::Number(n) => Self::Value(f32::from(n) as i32),
+            JsonValue::Array(v) => Self::List(v.into_iter().map(Into::into).collect()),
+            _ => unreachable!(),
+        }
+    }
+}
 
 fn main() {
-    let mut chunks = ReadChunks::new("input13.txt");
-    let mut map = chunks
-        .next()
-        .unwrap()
-        .into_iter()
-        .map(|line| {
-            let (x, y) = line.split_once(',').unwrap();
-            (x.parse().unwrap(), y.parse().unwrap())
-        })
-        .collect();
-
-    let ops = chunks.next().unwrap().into_iter().map(|line| {
-        let (dir, pos) = line.split_once('=').unwrap();
-        (dir.chars().last().unwrap(), pos.parse().unwrap())
-    });
-
-    for (dir, pos) in ops {
-        map = match dir {
-            'x' => fold_x(map, pos),
-            'y' => fold_y(map, pos),
-            _ => panic!("Unknown direction"),
-        };
-        println!("dir={} pos={} len={}", dir, pos, map.len());
-    }
-    print_map(map);
-}
-
-fn fold_y(v: HashSet<(i32, i32)>, pos: i32) -> HashSet<(i32, i32)> {
-    v.into_iter()
-        .map(|(x, y)| (x, if y <= pos { y } else { pos * 2 - y }))
-        .collect()
-}
-
-fn fold_x(v: HashSet<(i32, i32)>, pos: i32) -> HashSet<(i32, i32)> {
-    v.into_iter()
-        .map(|(x, y)| (if x <= pos { x } else { pos * 2 - x }, y))
-        .collect()
-}
-
-fn print_map(v: HashSet<(i32, i32)>) {
-    let mut min_x = std::i32::MAX;
-    let mut max_x = std::i32::MIN;
-    let mut min_y = std::i32::MAX;
-    let mut max_y = std::i32::MIN;
-    for &(x, y) in v.iter() {
-        min_x = std::cmp::min(min_x, x);
-        max_x = std::cmp::max(max_x, x);
-        min_y = std::cmp::min(min_y, y);
-        max_y = std::cmp::max(max_y, y);
-    }
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            if v.contains(&(x, y)) {
-                print!("#");
-            } else {
-                print!(" ");
-            }
+    let input = ReadChunks::new("input13.txt");
+    let mut sum = 0;
+    let k1 = Expr::parse("[[2]]");
+    let k2 = Expr::parse("[[6]]");
+    let mut all = vec![k1, k2];
+    for (lines, idx) in input.zip(1..) {
+        let left = Expr::parse(&lines[0]);
+        let right = Expr::parse(&lines[1]);
+        let cmp = left.cmp(&right);
+        if cmp == Ordering::Less {
+            sum += idx;
         }
-        println!();
+        all.push(left);
+        all.push(right);
     }
+    println!("{}", sum);
+    all.sort();
+    let key1 = all.binary_search(&Expr::parse("[[2]]")).unwrap() + 1;
+    let key2 = all.binary_search(&Expr::parse("[[6]]")).unwrap() + 1;
+    println!("{}", key1 * key2);
 }
