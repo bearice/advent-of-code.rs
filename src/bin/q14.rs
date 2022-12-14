60075a4b3e1f68adb25c241989633a77dbadc7fe
Bearice Ren <bearice@icybear.net>
2022-12-15 01:06:19 +0800

    2022-q14

--- src/bin/q14.rs
+++ src/bin/q14.rs
@@ -1,72 +1,138 @@
-use std::collections::HashMap;
+use std::{collections::HashSet, num::ParseIntError, str::FromStr};
 
-use advent_of_code::common::ReadChunks;
+use advent_of_code::common::read_lines;
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+    fn try_move(&self) -> [Point; 3] {
+        [
+            Self {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Self {
+                x: self.x - 1,
+                y: self.y + 1,
+            },
+            Self {
+                x: self.x + 1,
+                y: self.y + 1,
+            },
+        ]
+    }
+}
+
+impl FromStr for Point {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y) = s.split_once(',').unwrap();
+        Ok(Point {
+            x: x.parse()?,
+            y: y.parse()?,
+        })
+    }
+}
+
+fn parse_line(s: String, map: &mut HashSet<Point>) {
+    s.split("->")
+        .map(|s| s.trim().parse().unwrap())
+        .tuple_windows()
+        .for_each(|(from, to)| add_path(from, to, map));
+}
+
+fn add_path(from: Point, to: Point, map: &mut HashSet<Point>) {
+    // println!("add_path: {:?} -> {:?}", from, to);
+    if from.x == to.x {
+        for y in from.y.min(to.y)..from.y.max(to.y) + 1 {
+            map.insert(Point { x: from.x, y });
+        }
+    } else if from.y == to.y {
+        for x in from.x.min(to.x)..from.x.max(to.x) + 1 {
+            map.insert(Point { x, y: from.y });
+        }
+    } else {
+        unreachable!()
+    }
+}
+
+fn add_sand(origin: Point, limit: i32, bottom: Option<i32>, map: &mut HashSet<Point>) -> bool {
+    if map.contains(&origin) {
+        return false;
+    }
+    let mut new = origin;
+    while new.y < limit {
+        let mut next = None;
+        for p in new.try_move() {
+            if map.contains(&p) || Some(p.y) == bottom {
+                continue;
+            }
+            next = Some(p);
+            break;
+        }
+        if let Some(next) = next {
+            new = next;
+        } else {
+            map.insert(new);
+            return true;
+        }
+    }
+    false
+}
 
 fn main() {
-    let mut chunks = ReadChunks::new("input14.txt");
-    let template = chunks.next().unwrap();
-    let pairs = chunks.next().unwrap();
-    let chars = template[0].as_bytes();
-    let mut template =
-        chars
-            .iter()
-            .zip(chars.iter().skip(1))
-            .fold(HashMap::new(), |mut acc, (&a, &b)| {
-                *acc.entry((a, b)).or_insert(0) += 1;
-                acc
-            });
-
-    // add markers of head and tail
-    template.insert((0, chars[0]), 1);
-    template.insert((chars[chars.len() - 1], 0), 1);
-
-    let pairs = pairs
-        .iter()
-        .map(|s| {
-            let s = s.as_bytes();
-            ((s[0], s[1]), s[6])
-        })
-        .collect();
-
-    for i in 1..=40 {
-        round(&mut template, &pairs);
-        if i == 10 || i == 40 {
-            println!("count[{}] = {}", i, count(&template));
+    let mut map = HashSet::new();
+    read_lines("input14.txt").for_each(|line| parse_line(line, &mut map));
+    let limit = map.iter().max_by_key(|p| p.y).unwrap().y;
+    {
+        let mut map = map.clone();
+        let origin = Point::new(500, 0);
+        let mut count = 0;
+        while add_sand(origin, limit, None, &mut map) {
+            count += 1;
         }
+        println!("{}", count);
+    }
+    {
+        let mut map = map;
+        let origin = Point::new(500, 0);
+        let mut count = 0;
+        while add_sand(origin, limit + 3, Some(limit + 2), &mut map) {
+            count += 1;
+        }
+        println!("{}", count);
     }
 }
 
-fn count(template: &HashMap<(u8, u8), usize>) -> usize {
-    let mut count = template
-        .iter()
-        .fold(HashMap::new(), |mut acc, (&(c1, c2), &count)| {
-            *acc.entry(c1).or_insert(0) += count;
-            *acc.entry(c2).or_insert(0) += count;
-            acc
-        });
-
-    // remove head and tail markers
-    count.remove(&0);
-
-    // each char was counted twice, so divide by 2
-    count.iter_mut().for_each(|(_, v)| *v /= 2);
-
-    // println!("{:?}", count);
-    let max = count.iter().max_by_key(|&(_, &v)| v).unwrap();
-    let min = count.iter().min_by_key(|&(_, &v)| v).unwrap();
-    max.1 - min.1
-}
-
-fn round(template: &mut HashMap<(u8, u8), usize>, pairs: &HashMap<(u8, u8), u8>) {
-    let mut ret = HashMap::new();
-    for (&(a, b), &count) in template.iter() {
-        let c = pairs.get(&(a, b));
-        if let Some(&c) = c {
-            *ret.entry((a, c)).or_insert(0) += count;
-            *ret.entry((c, b)).or_insert(0) += count;
-        } else {
-            ret.insert((a, b), count);
-        }
+#[allow(dead_code)]
+fn print_map(v: &HashSet<Point>) {
+    let mut min_x = std::i32::MAX;
+    let mut max_x = std::i32::MIN;
+    let mut min_y = std::i32::MAX;
+    let mut max_y = std::i32::MIN;
+    for &p in v {
+        min_x = std::cmp::min(min_x, p.x);
+        max_x = std::cmp::max(max_x, p.x);
+        min_y = std::cmp::min(min_y, p.y);
+        max_y = std::cmp::max(max_y, p.y);
     }
-    *template = ret
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            if v.contains(&Point { x, y }) {
+                print!("#");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
 }
